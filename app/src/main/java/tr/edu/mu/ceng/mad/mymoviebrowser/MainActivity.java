75e42af95976d24ebf8fdc02dbc52d3a1ffbb4f9
u190709027 <ogulcanbulut3@posta.mu.edu.tr>
2021-11-12 17:03:49 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/mymoviebrowser/MainActivity.java
@@ -0,0 +1,19 @@
+package tr.edu.mu.ceng.mad.mymoviebrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements MovieFragment.OnMovieSelected {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void movieSelected(Movie movie) {
+
+    }
+}
---
+++ app/src/main/java/tr/edu/mu/ceng/mad/mymoviebrowser/MyMovieRecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package tr.edu.mu.ceng.mad.mymoviebrowser;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import tr.edu.mu.ceng.mad.mymoviebrowser.placeholder.PlaceholderContent.PlaceholderItem;
+import tr.edu.mu.ceng.mad.mymoviebrowser.databinding.FragmentMovieBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyMovieRecyclerViewAdapter extends RecyclerView.Adapter<MyMovieRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Movie> mValues;
+    private MovieFragment.OnMovieSelected listener;
+    int selectedIndex;
+
+
+    public MyMovieRecyclerViewAdapter(List<Movie> items, MovieFragment.OnMovieSelected listener) {
+        mValues = items;
+        this.listener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentMovieBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(position+"");
+        holder.mContentView.setText(mValues.get(position).getName());
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.movieSelected(holder.mItem);
+                    notifyItemChanged(selectedIndex);
+                    selectedIndex = holder.getLayoutPosition();
+                    notifyItemChanged(selectedIndex);
+                }
+            }
+        });
+        holder.itemView.setBackgroundColor(selectedIndex == position ? Color.GREEN : Color.TRANSPARENT);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public Movie mItem;
+
+        public ViewHolder(FragmentMovieBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
